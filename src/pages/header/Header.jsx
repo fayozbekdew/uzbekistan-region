8886fdc2d11c8417151afc2efa6b7ae3fb42df91
fayozbekdew <fayozbekdev@gmail.com>
2023-06-27 17:20:13 +0500

    flag

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="header">
     <div className="container">
         <a className="header__logo" href="./index.html">
-          <img src="./public/images/Uzbekistan-Map-Flag-With-Stroke.svg" alt="uzb-flags"width={100} height={80}/>
+          <img src="./images/Uzbekistan-Map-Flag-With-Stroke.svg" alt="uzb-flags"width={100} height={80}/>
         </a>
         <Link className='home' to='/'>Bosh sahifa</Link>
         <Mode/>
